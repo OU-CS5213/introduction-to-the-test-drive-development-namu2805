56a67d3300c3d0fa5a418b764e9f7e9da64bfd4c
namu2805 <barbara.namulwana-1@ou.edu>
2021-03-04 20:11:30 -0600

    Update AWS.java
    updated file commit

--- AWS.java
+++ AWS.java
@@ -1,29 +1,28 @@
 import java.util.Arrays;
 
 public class AWS {
-	private static final int FILLER_VALUE = Integer.MIN_VALUE;
+	public static final int FILLER_VALUE = Integer.MIN_VALUE;
 	private int[] values;
-
+	
 	public int[] getValues() {
 		return Arrays.copyOf(values, values.length);
 	}
-
+	
 	public void setValues(int[] values) {
 		this.values = Arrays.copyOf(values, values.length);
 	}
-
+	
 	@Override
 	public String toString() {
 		return "AWS [values=" + Arrays.toString(values) + "]";
 	}
-
+	
 	public AWS(int[] values) {
 		super();
 		setValues(values);
 	}
-
+	
 	public int remove(int i) {
-
 		int value = FILLER_VALUE;
 		if (i >= 0 && i < values.length) {
 			value = values[i];
@@ -34,26 +33,48 @@ public class AWS {
 		}
 		return value;
 	}
-
+	
 	public void fillAndExpand(int position, int nt) {
 		int numberOfTimes = Math.abs(nt);
 		int[] newArray = new int[values.length + numberOfTimes];
-		for(int i = 0; i <=position; ++i) {
+		for(int i = 0; i <= position; ++i) {
 			newArray[i] = values[i];
 		}
-		for(int i = position; i<=numberOfTimes + position; ++i) {
+		for(int i = position; i <= numberOfTimes + position; ++i) {
 			newArray[i] = newArray[position];
 		}
-		// Make examples, so it is easier to visualize your methods
-		//   p 1 2
-		// a b c
-		// a b b b c
-		
 		int x = numberOfTimes;
-		for(int i = position+1; i <values.length; ++i ) {
+		for(int i = position+1; i < values.length; ++i) {
 			newArray[i+x] = values[i];
 		}
 		values = newArray;
 	}
+
+	public int removeBiggerThan(int threshold) {
+		int numCount = 0;
+		for(int i = 0; i < values.length; ++i) {
+			if(values[i] > threshold) {
+				values[i] = FILLER_VALUE;
+				numCount += 1;
+			}
+		}
+		return numCount;
+	}
+
+	public void stepMultiplier() {
+		for(int i = 0; i < values.length; ++i) {
+			if(values[i] < 100 && values[i] > 20) {
+				values[i] = values[i]*100;
+			}
+			else if(values[i] < 20 && values[i] > 10) {
+				values[i] = values[i]*4;
+			}
+			else if(values[i] < 10) {
+				values[i] = values[i]*2;
+			}
+		}
+		
+	}
+	
 
 }
